984102c3f89168c113606eeb7581e0c3ed936a71
Usuario <Usuario@cvaros>
2018-03-29 18:19:51 -0600

    Crud Usuario, login, registro

--- src/java/org/test/ControllerEmpresa.java
+++ src/java/org/test/ControllerEmpresa.java
@@ -13,14 +13,23 @@ import servicios.ServicioEmpresa;
 import entidades.Empresa;
 import entidades.Empresacliente;
 import entidades.Empresausuario;
+import entidades.Plan;
+import java.io.InputStream;
+import java.io.Serializable;
+import java.util.Arrays;
 import java.util.List;
+import javax.annotation.PostConstruct;
 import servicios.ServicioEmpresacliente;
-import servicios.ServicioEmpresausuario;import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import servicios.ServicioEmpresausuario;
+import servicios.ServicioPlan;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.servlet.http.Part;
+import org.apache.commons.io.IOUtils;
 
 @ManagedBean(name = "controllerEmpresa")
-@RequestScoped
-public class ControllerEmpresa {
+@ViewScoped
+public class ControllerEmpresa implements Serializable {
 
     ServicioEmpresa servicioEmpresa = new ServicioEmpresa();
     ServicioEmpresausuario sEU = new ServicioEmpresausuario();
@@ -28,54 +37,180 @@ public class ControllerEmpresa {
     Empresa empresa = new Empresa();
     Empresausuario eU = new Empresausuario();
     Empresacliente eC = new Empresacliente();
+    List<Empresa> listCompany;
+    List<Plan> listPlan;
+    ServicioPlan servicePlan = new ServicioPlan();
+    Plan plan = new Plan();
+    Part file;
 
-    public boolean insertarEmpresa(Empresa e) throws Exception {
-        if (e != null) {
-            return false;
-        } else {
-            servicioEmpresa.insert(e);
-            return true;
+    public void insertarEmpresa() throws Exception {
+        try {
+            plan = servicePlan.read(plan);
+            
+            this.empresa.setPlan(plan);
+            this.empresa.setCantidadRestanteFacturas(plan.getCantidad());
+
+            if (partToByteArray() != null) {
+                this.empresa.setLogo(partToByteArray());
+            }
+            
+            servicioEmpresa.insert(this.empresa);
+            empresa = new Empresa();
+            file = null;
+            plan = new Plan();
+        } catch (Exception e) {
+            System.out.println("Error al insertar.");
+            System.out.println(e);
         }
     }
 
-    public boolean modificarEmpresa(Empresa e) throws Exception {
-        if (e != null) {
-            return servicioEmpresa.modify(e) != false;
-        } else {
-            return false;
+    private byte[] partToByteArray() {
+        byte[] logo;
+        try {
+            InputStream input = file.getInputStream();
+            logo = IOUtils.toByteArray(input);
+            return logo;
+        } catch (Exception e) {
+            System.out.println("El logo esta vacia o no a sido seleccionado.");
+            System.out.println(e);
+            return null;
         }
     }
 
-    public Empresa leerEmpresa(Empresa e) throws Exception {
-        return servicioEmpresa.read(e);
+    public void modificarEmpresa(int companyId, String companyName, int companyCedula, String companyPersona,
+            int companyTelefono, String companyEmail, int companyConsecutivo, String companyEstado, int companyPlanId) throws Exception {
+        try {
+            this.plan.setId(companyPlanId);
+
+            plan = new Plan();
+            empresa = new Empresa();
+            file = null;
+            
+            this.empresa.setId(companyId);
+            this.plan.setId(companyPlanId);
+
+            plan = servicePlan.read(plan);
+            empresa = servicioEmpresa.read(empresa);
+
+            if (this.empresa.getPlan().getId() == this.plan.getId()) {
+                System.out.println("Same plan.");
+            } else {
+                this.empresa.setCantidadRestanteFacturas(this.empresa.getCantidadRestanteFacturas() + this.plan.getCantidad());
+            }
+
+            this.empresa.setNombre(companyName);
+            this.empresa.setCedula(companyCedula);
+            this.empresa.setTipo_Persona(companyPersona);
+            this.empresa.setTelefono(companyTelefono);
+            this.empresa.setCorreo(companyEmail);
+            this.empresa.setConsecutivo_Ini(companyConsecutivo);
+            this.empresa.setEstado(companyEstado);
+            this.empresa.setPlan(plan);
+
+            if (partToByteArray() != null) {
+                this.empresa.setLogo(partToByteArray());
+            }
+
+            servicioEmpresa.modify(empresa);
+
+            empresa = new Empresa();
+            file = null;
+            plan = new Plan();
+            
+            System.out.println("Modificado.");
+        } catch (Exception e) {
+            System.out.println("Error al modificar.");
+            System.out.println(e);
+        }
     }
 
-    public boolean eliminarEmpresa(Empresa e) throws Exception {
-        if (e != null) {
-            if (servicioEmpresa.list(e) != null) {
+    public void linkCompany(Empresa company) throws Exception {
+        this.empresa = company;
+    }
+
+    public Empresa leerEmpresa() throws Exception {
+        return servicioEmpresa.read(empresa);
+    }
+
+    public void eliminarEmpresa(int companyId) throws Exception {
+        boolean result = true;
+        Empresa company = new Empresa();
+        System.out.println(companyId);
+        company.setId(companyId);
+        if (companyId != 0) {
+            if (servicioEmpresa.list(company) != null) {
+                company = servicioEmpresa.read(company);
                 for (Empresausuario em : sEU.list(eU)) {
-                    if (em.getEmpresa().getId()== e.getId()) {
-                        return false;
+                    if (em.getEmpresa().getId() == company.getId()) {
+                        result = false;
+                        System.out.println("La empresa tiene datos ligados.");
                     }
                 }
                 for (Empresacliente ec : sEC.list(eC)) {
-                    if (ec.getEmpresa().getId()== e.getId()) {
-                        return false;
+                    if (ec.getEmpresa().getId() == company.getId()) {
+                        result = false;
+                        System.out.println("La empresa tiene datos ligados.");
                     }
-
                 }
-                e.setEstado("I");
-                servicioEmpresa.modify(e);
-                return true;
+                if (result = true) {
+                    company.setEstado("I");
+                    servicioEmpresa.modify(company);
+                    System.out.println("Eliminado.");
+                    empresa = new Empresa();
+                    file = null;
+                    plan = new Plan();
+                }
             } else {
-                return false;
+                System.out.println("La empresa no existe.");
             }
         } else {
-            return false;
+            System.out.println("No ha seleccionado ninguna empresa.");
         }
     }
-    
-    public List<Empresa> listaEmpresa() throws Exception{
-        return servicioEmpresa.list(empresa);
+
+    @PostConstruct
+    private void charge() {
+        try {
+            this.listCompany = this.servicioEmpresa.list(this.empresa);
+            this.listPlan = this.servicePlan.list(this.plan);
+        } catch (Exception e) {
+            System.out.println("Error al cargar los datos.");
+        }
+    }
+
+    public List<Empresa> getListCompany() {
+        return listCompany;
+    }
+
+    public Empresa getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(Empresa empresa) {
+        this.empresa = empresa;
+    }
+
+    public Plan getPlan() {
+        return plan;
+    }
+
+    public void setPlan(Plan plan) {
+        this.plan = plan;
+    }
+
+    public List<Plan> getListPlan() {
+        return listPlan;
+    }
+
+    public void setListPlan(List<Plan> listPlan) {
+        this.listPlan = listPlan;
+    }
+
+    public Part getFile() {
+        return file;
+    }
+
+    public void setFile(Part file) {
+        this.file = file;
     }
 }
