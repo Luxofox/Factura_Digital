4152a9701451ec15eb3c789cb76667d43293bdd8
LiuJaime <liuli0997@gmail.com>
2018-04-02 17:27:04 -0600

    jaime

---
+++ src/java/org/test/empresaConverter.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.test;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.ConverterException;
+import javax.faces.convert.FacesConverter;
+import servicios.ServicioEmpresa;
+
+/**
+ *
+ * @author Li
+ */
+@FacesConverter(value = "empresaConverter")
+public class empresaConverter implements Converter {
+    
+    private ServicioEmpresa sp;
+    
+    public empresaConverter() {
+        sp = new ServicioEmpresa();
+    }
+
+@Override
+    public Object getAsObject(FacesContext facesContext, UIComponent component,
+            String submittedValue) {
+        if (submittedValue != null && submittedValue.trim().length() > 0) {
+            try {
+                return sp.findByPK(Integer.parseInt(submittedValue));
+            } catch (Exception exception) {
+                throw new ConverterException(new FacesMessage(FacesMessage.SEVERITY_ERROR, "Conversion Error", "No es una empresa válida."));
+            }
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public String getAsString(FacesContext facesContext, UIComponent component,
+            Object value) {
+        if (value == null || value.equals("")) {
+            return "";
+        } else {
+            return String.valueOf(value);
+        }
+    }
+}
